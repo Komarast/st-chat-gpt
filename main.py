3e1bddd5adc9c21ca74c53c618a524cdd3541b5f
tomas.komaras@everwestgroup.net <tomas.komaras@everwestgroup.net>
2024-12-30 12:48:26 +0200

    fixed bug

--- main.py
+++ main.py
@@ -24,23 +24,29 @@ if user not asking for any framework soluton (langchain, crewAi, etc.) provide u
 
 
 
-
+st.set_page_config(page_title="My chat AI")
 st.title("AI Chat")
 
 if "messages" not in st.session_state:
     st.session_state.messages = []
-    st.session_state.messages.append({"role":"user","content": prompt})
+    st.session_state.messages.append({"role": "system", "content": system_prompt})
 
 for message in st.session_state.messages:
+    if message["role"] == "system":
+        continue
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
 
+
+
 if prompt := st.chat_input("What is up?"):
     with st.chat_message("user"):
+        st.session_state.messages.append({"role":"user","content": prompt})
         st.markdown(prompt)
 
+
     with st.chat_message("assistant"):
-        st.session_state.messages.append({"role": "system", "content": system_prompt})
+        
         stream = client.chat.completions.create(
         model = st.session_state["openai_model"],
         messages=[
